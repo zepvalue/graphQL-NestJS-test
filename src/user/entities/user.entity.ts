3883d836eb23f18e0ee2c6cde2129a6a12e95f3b
zepvalue <zepvalue@gmail.com>
2020-12-31 19:10:33 +0100

    adding user resource

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class User {
+  @Field(() => Int, { description: 'Example field (placeholder)' })
+  id: number;
+
+  @Field()
+  timeIn: number;
+
+  @Field()
+  timeOut: number;
+}
